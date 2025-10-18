b816ecd71378f769445f3a159e6c18a5e3444759
Chris Coyier <chriscoyier@gmail.com>
2025-10-18 07:50:09 -0700

    updates

---
+++ wp-content/plugins/wordpress-seo/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '21a310db0ea5ac3cc425314a6b286f8042054bd8',
+        'name' => 'yoast/wordpress-seo',
+        'dev' => false,
+    ),
+    'versions' => array(
+        'composer/installers' => array(
+            'pretty_version' => 'v2.3.0',
+            'version' => '2.3.0.0',
+            'type' => 'composer-plugin',
+            'install_path' => __DIR__ . '/./installers',
+            'aliases' => array(),
+            'reference' => '12fb2dfe5e16183de69e784a7b84046c43d97e8e',
+            'dev_requirement' => false,
+        ),
+        'yoast/wordpress-seo' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '21a310db0ea5ac3cc425314a6b286f8042054bd8',
+            'dev_requirement' => false,
+        ),
+    ),
+);
