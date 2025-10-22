06be1a7d7a7cbe7af468acaf214bfa0806170391
Chris Coyier <chriscoyier@gmail.com>
2025-10-22 13:18:17 -0700

    updates

--- wp-content/themes/april-coppini/functions.php
+++ wp-content/themes/april-coppini/functions.php
@@ -2,7 +2,7 @@
 
   // The entire sites' CSS
   if (!is_admin() && ($GLOBALS['pagenow'] !== 'wp-login.php')) {
-    wp_enqueue_style('april-coppini-style', get_template_directory_uri() . '/style.css', array(), 'cache_bust=6546612' );
+    wp_enqueue_style('april-coppini-style', get_template_directory_uri() . '/style.css', array(), 'cache_bust=6546613' );
   }
 
   add_theme_support('menus');
